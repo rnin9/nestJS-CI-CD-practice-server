250f8956ea2980fcca9c7b671ee0d06bdf000f1a
rnin9 <rkdalswn1209@kw.ac.kr>
2021-07-12 16:26:21 +0900

    check ver2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Dev & CI/CD World!';
+    return 'Hello Dev & CI/CD ver.2 World!';
   }
 }
